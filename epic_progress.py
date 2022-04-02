ec7022c21ed9515f5266690378fb642d4c2f902e
Rahul.Jain <rahulj51@gmail.com>
2022-04-02 20:54:38 +0530

    Using duckdb

--- epic_progress.py
+++ epic_progress.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import duckdb
 from annotated_text import annotated_text
 from st_aggrid import AgGrid
 
@@ -15,19 +16,19 @@ done_statuses = ['Done - In production']
 uploaded_file = st.file_uploader("Choose a file")
 
 
-def split_into_epics_and_tickets(df):
-    df1, df2 = df[(mask := df['Issue Type'] == 'Epic')], df[~mask]
-    return df1, df2
+def split_into_epics_and_tickets(alldata):
+    epics = alldata.filter('"Issue Type" = \'Epic\'')
+    tickets = alldata.filter('"Issue Type" != \'Epic\'')
+    return epics, tickets
 
 
-def summary_status(tickets, epic_status):
+def summary_status(ticket_statuses, epic_status):
     # we shouldn't trust the epic status since no one changes it usually
     # but let's use it as the default
-    summary_status = 'Unknown' if epic_status.empty  else epic_status.iloc[0]
+    summary_status = epic_status or 'Unknown'
 
-    if tickets.size > 0:
-        statuses = tickets['Status'].to_list()
-        if any(status in statuses for status in in_progress_statuses):
+    if len(ticket_statuses) > 0:
+        if any(x in ticket_statuses for x in in_progress_statuses):
             summary_status = 'In Progress'
         else:
             summary_status = 'Completed'
@@ -43,53 +44,49 @@ def get_color(status):
     return color
 
 
-def pct_completed(tickets):
+def pct_completed(ticket_statuses):
     pct_completed = 0
-    total_count = tickets.size
-    completed_count = tickets[tickets['Status'].isin(done_statuses)].size
+    total_count = len(ticket_statuses)
+    completed_count = len([x for x in ticket_statuses if x in done_statuses])
     if total_count > 0:
         pct_completed = (completed_count / total_count) * 100
 
     return round(pct_completed)
 
 
-
-
-
 if uploaded_file is not None:
     full_df = pd.read_csv(uploaded_file)
-    # st.write(full_df)
-    # st.subheader("filtered")
     df = full_df[keys]
-    # st.write(df)
+    conn = duckdb.connect()
+
+    alldata = conn.from_df(df)
 
     # split into epics and tickets
-    epics, tickets = split_into_epics_and_tickets(df)
+    epics, tickets = split_into_epics_and_tickets(alldata)
     # st.subheader("Epics")
-    # st.write(epics)
+    # st.write(epics.df())
     # st.subheader("Tickets")
-    # st.write(tickets)
+    # st.write(tickets.df())
 
-    # now group tickets by epic link
-    tickets_by_epic = tickets.groupby('Custom field (Epic Link)')
-    epic_keys = tickets_by_epic.groups.keys()
+    # iterate over each epic
+    epic_links = [ x[0] for x in epics.project('"Issue key"').order('"Issue key"').fetchall()]
+    for epic in epic_links:
+        epic_details = epics.filter('"Issue key" = \'{}\''.format(epic)).fetchall()[0]
+        epic_summary = epic_details[0]
+        epic_status = epic_details[3]
 
-    # for each group, use some logic for completion status
-    for epic in epic_keys:
-        epic_details = epics.loc[epics['Issue key'] == epic].head(1)
-        epic_status = epic_details['Status']
-        epic_summary = epic_details['Summary'].iloc[0] if not epic_details.empty else 'Unknown'
-        tickets_in_this_epic = tickets_by_epic.get_group(epic)
-        status = summary_status(tickets_in_this_epic, epic_status)
-        pct = pct_completed(tickets_in_this_epic)
+        tickets_in_this_epic = tickets.filter('"Custom field (Epic Link)" = \'{}\''.format(epic))
+        ticket_statuses = [x[0] for x in tickets_in_this_epic.project('Status').fetchall()]
+        status = summary_status(ticket_statuses, epic_status)
+        pct = pct_completed(ticket_statuses)
+
         with st.container():
             annotated_text((f"{status}", "", f"{get_color(status)}"))
             st.markdown(f"Epic - [{epic}](https://jira.taxibeat.com/browse/{epic}) : {epic_summary}")
             st.progress(pct)
             with st.expander("See all tickets"):
-                AgGrid(tickets_in_this_epic)
+                AgGrid(tickets_in_this_epic.to_df(), key=epic)
 
-        # use agggrid for better visualization
 
 
 
